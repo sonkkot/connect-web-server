bcd8ac35a15d596a68763053abbd7ba277e7287c
lkdcode <dlrlejr1@naver.com>
2024-07-28 19:30:40 +0900

    Initial commit

---
+++ src/main/java/site/copi/CopiApplication.java
@@ -0,0 +1,11 @@
+package site.copi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CopiApplication.class, args);
+    }
+}
