d437de8d94779b46f64b15a03fe71bdd3230b6c9
lkdcode <dlrlejr1@naver.com>
2024-08-04 22:57:15 +0900

    Feat(:boot) add boot module

---
+++ copi-web-boot/src/main/java/site/copi/CopiSpringBootApplication.java
@@ -0,0 +1,11 @@
+package site.copi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CopiSpringBootApplication.class, args);
+    }
+}
