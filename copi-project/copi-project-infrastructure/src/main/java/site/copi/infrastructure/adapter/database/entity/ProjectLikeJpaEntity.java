b81cb28ded7db414bf53e4b907a55ee2441dc458
lkdcode <dlrlejr1@naver.com>
2024-11-11 22:55:28 +0900

    Feat(:project) add projectJpaEntity

---
+++ copi-project/copi-project-infrastructure/src/main/java/site/copi/infrastructure/adapter/database/entity/ProjectLikeJpaEntity.java
@@ -0,0 +1,46 @@
+package site.copi.infrastructure.adapter.database.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.copi.project.domain.value.ProjectId;
+import site.copi.project.domain.value.ProjectWriter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@Table(name = "TB_PROJECT")
+@NoArgsConstructor(access = PROTECTED)
+public class ProjectLikeJpaEntity {
+
+    @Positive
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Positive
+    @Column(nullable = false)
+    private Long userId;
+
+    @Positive
+    @Column(nullable = false)
+    private Long projectId;
+
+    @Builder
+    public ProjectLikeJpaEntity(Long id, Long userId, Long projectId) {
+        this.id = id;
+        this.userId = userId;
+        this.projectId = projectId;
+    }
+
+    @Builder(builderMethodName = "aggregateBuilder")
+    public ProjectLikeJpaEntity(ProjectId id, ProjectWriter userId, ProjectId projectId) {
+        this.id = id.id();
+        this.userId = userId.id();
+        this.projectId = projectId.id();
+    }
+}
